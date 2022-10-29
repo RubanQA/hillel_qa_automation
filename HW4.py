0aeba19acba7ea709a16cf5b96fa9f03a398c66d
Denis Ruban <nonkepz@gmail.com>
2022-10-29 18:12:21 +0300

    add HW4

--- HW4.py
+++ HW4.py
@@ -1,12 +1,27 @@
-n = 5
-i = 1
-while n > 0:
+# 1. Вывести треугольник #1 с шириной N с помощью цикла while
+n = int(input('Please enter width N: '))
+i = n
+while i > 0:
   print(i * '*')
   i -= 1
 
+# 2. Вывести треугольник #2 с шириной N с помощью цикла while
+n = int(input('Please enter width N: '))
+i = 1
+while i <= n:
+  print(i * '*')
+  i += 1
 
-# n = 5
-# i = 1
-# while i < n + 1:
-#   print(i * '*')
-#   i += 1
+# 3. Вывести треугольник #3 с шириной N с помощью цикла while
+n = int(input('Please enter width N: '))
+i = n
+while i > 0:
+  print(' '*(n-i) + '*' * i)
+  i -= 1
+
+# 4. Вывести треугольник #4 с шириной N с помощью цикла while
+n = int(input('Please enter width N: '))
+i = 1
+while i <= n:
+  print(' '*(n-i) + '*' * i)
+  i += 1
