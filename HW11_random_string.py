29835ba8b678875b77c3492490a4337a00a56208
Denis Ruban <nonkepz@gmail.com>
2022-12-08 02:33:18 +0200

    update HW11_random_string.py

--- HW11_random_string.py
+++ HW11_random_string.py
@@ -4,17 +4,19 @@
 # - Не использовать модуль string
 # - Не создавать руками список ['a', 'b', 'c', ..., 'X', 'Y', 'Z', 0, 1, ..., 8, 9]
 
-import random
+from random import randint, choice
 
 
 def get_random_string(length: int) -> str:
-    r_string = ""
+    list_char = []
+    random_string = ''
     for i in range(length):
-        numbers = chr(random.randint(48, 57))
-        char_upper = chr(random.randint(97, 123))
-        char_lower = chr(random.randint(65, 91))
-        r_string += char_upper + char_lower + numbers
-    return r_string
+        list_char.append(chr(randint(65, 90)))  # Upper letters
+        list_char.append(chr(randint(97, 122)))  # Lower letters
+        list_char.append(chr(randint(48, 57)))  # Numbers
+        random_char = choice(list_char)
+        random_string += random_char
+    return random_string
 
 
-print(get_random_string(5))
+print(get_random_string(10))
