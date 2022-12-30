881596da58282f496c4110e6596c63a767c703d9
Denis Ruban <nonkepz@gmail.com>
2022-12-31 01:14:28 +0200

    update HW16_basic_classes.py

--- HW16_basic_classes.py
+++ HW16_basic_classes.py
@@ -10,108 +10,123 @@
 
 from datetime import date
 
+
 class Company:
+    count = 0
+    amount = []
 
     """Company model"""
-    def _init_(self, name: str, country: str, foundation_year: int, employees: int, projects: int):
+
+    def __init__(self, company_name: str, foundation_year: int, country: str, employees_number: int, income: int):
         """Initialization company attributes"""
-        self.name = name
-        self.country = country
+        self.company_name = company_name
         self.foundation_year = foundation_year
-        self.employees = employees
-        self.projects = projects
+        self.country = country
+        self.employees_number = employees_number
+        self.income = income
+        Company.count += 1
+        Company.amount.append(income)
 
     """count company age"""
+
     def company_age(self):
         now = date.today().year
         return now - self.foundation_year
 
-    """average number of employees on the projects"""
-    def avg_employees_on_projects(self, employees, projects):
-        return self.employees // self.projects
+    """Show company info"""
 
-    """average company income"""
+    def view_conpany_info(self):
+        print(f'Company name: {self.company_name}\nAge: {self.company_age()}\nCompany country: {self.country} '
+              f'\ncompany income: {self.income}')
+
+    """Avg income company"""
+
+    @classmethod
+    def get_avg_income(cls, *income: list):
+        summ = sum(income[0])
+        avg = round(summ / Company.count, 2)
+        print(f'Income companies: {avg}')
+        return avg
+
+    """Avg salary"""
+
     @staticmethod
-    def avg_year_income(self):
-        return self.avg_year_income // 12
+    def get_avg_salary(income, employees):
+        salary = income / employees
+        print(f"Avg salary of company: {salary}")
 
-    """Add field to class"""
-    @classmethod
-    def add_class_attribute(cls, attr, value=None):
-        if not hasattr(cls, attr):
-            setattr(cls, attr, value)
-            return f'{attr} attribute is added to class {cls.__name__}'
-        else:
-            return f'Attribute {attr} exists already'
 
-    """Check attribute in class"""
-    @classmethod
-    def check_attribute(self, attr):
-        self.attr = attr
-        return hasattr(self, attr)
+itrex = Company("ITrex", 2011, "Ukraine", 300, 700000)
+m2e = Company("M2E", 2005, "Ukraine", 50, 300000)
+epam = Company("Epam", 2002, "Ukraine", 10000, 12700000)
 
-    """delete class field"""
-    @classmethod
-    def delete_class_attribute(cls, attr):
-        delattr(cls, attr)
+companies = [itrex, m2e, epam]
+rates = [company.income for company in companies]
 
-    """about company"""
-    def company_info(self):
-        print(f'Company name: {self.name}')
-        print(f'Company name: {self.country}')
-        print(f'Company age: {self.company_age()}')
-        print(f'Employees: {self.employees}')
-        print(f'Active projects: {self.projects}')
+itrex.view_conpany_info()
+m2e.view_conpany_info()
+epam.view_conpany_info()
 
+Company.get_avg_salary(200000, 300)
+Company.get_avg_income(rates)
 
 
 class Employee:
-    department = 'Web Development'
-    count = 0
+    empAmount = 0
+    overtime_bonus = 0
 
-    def __init__(self, name, surname, experience, salary):
-        """Initialization company attributes"""
+    'Constructor'
+
+    def __init__(self, name, years, specialty, salary, ):
         self.name = name
-        self.surname = surname
-        self.work_experience = experience
+        self.years = years
+        self.specialty = specialty
         self.salary = salary
-        self.english_level = 'upper-intermediate'
-        Employee.count += 1
+        Employee.empAmount += 1
 
-    def change_name(self, new_name):
-        """Change name employee"""
-        self.name = new_name
-        print(f'New name: {self.name}')
+    'The method shows the employee`s salary'
 
-    def change_surname(self, new_surname):
-        """Change surname employee"""
-        self.surname = new_surname
-        print(f'New surname: {self.surname}')
+    def show_emp_salary(self):
+        print(f'Name : {self.name}, Salary: {self.salary}')
 
-    def change_english_level(self, new_english_level):
-        """Change english level of employee"""
-        self.english_level = new_english_level
-        print(f'New english level: {self.english_level}')
+    'The method shows the employee`s specialty'
 
-    def print_employee_info(self):
-        """Info about employee"""
-        print(
-            f'Name: {self.name},\nSurname: {self.surname},\nExpirence: {self.work_experience},\nSalary: {self.salary},\nDepartment: {Employee.department}\n')
+    def show_emp_specialty(self):
+        print(f'Name : {self.name}, Specialty: {self.specialty}')
 
-    @staticmethod
-    def get_salary_total(*salary: list):
-        """Total salary budget"""
-        summ = sum(salary[0])
-        print(f'Salary budget: {summ}')
-        return summ
+    'The class method show the overtime bonus'
 
     @classmethod
-    def change_raise_salary(cls, new_value):
-        """Check raise salary"""
-        cls.year_salary_raise_value = new_value
-        return new_value
+    def show_overtime_bonus_for_employees(cls):
+        print(f'Bonus amount for overtime: {cls.overtime_bonus}')
+
+    'The method shows the number of all employees'
 
     @staticmethod
-    def start_work():
-        """View start working"""
-        print('Start.')
+    def show_emp_amount():
+        print(f'Total Employee: {Employee.empAmount}')
+
+    'The method shows the assigned tasks by specialties'
+
+    @staticmethod
+    def assigned_task(specialty):
+        print('Your assigned tasks:')
+        if specialty == 'Dev':
+            requirement = ['dev_task_1', 'dev_task_2', 'dev_task_3']
+        elif specialty == 'QA':
+            requirement = ['qa_task_1', 'qa_task_2']
+        elif specialty == 'PM':
+            requirement = ['pm_task_1', 'pm_task_2']
+        else:
+            requirement = ['other_task_1']
+        return requirement
+
+
+emp_Denis = Employee("Denis", 32, "QA", 3000)
+emp_Denis.show_emp_amount()
+emp_Denis.show_emp_salary()
+emp_Denis.show_emp_specialty()
+print(emp_Denis.assigned_task("Dev"))
+Employee.overtime_bonus = 1000
+Employee.show_overtime_bonus_for_employees()
+emp_Denis.show_overtime_bonus_for_employees()
